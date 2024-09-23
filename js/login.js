63cc1d47dd4965260a661f30586d4f0aab91a61e
mohtashim haider <mohtashimhaider@mohtashims-MacBook-Air.local>
2024-09-23 13:44:08 +0600

    login validation checked

--- js/login.js
+++ js/login.js
@@ -5,6 +5,15 @@ document.getElementById('login-button').addEventListener('click',(e)=>{
     //getting input from the form
     const phoneNumber = document.querySelector('#phone-number').value;
     const pinNumber = document.querySelector('#pin').value;
+
+    //login validation check
+
+    if(phoneNumber==='5' && pinNumber==='1234'){
+        window.location.href= '/home.html'
+    }
+    else{
+        alert('fail');
+    }
     
 })
 
