ba9065a9c516a0baee38cba6de06923c572e4cf4
mohtashim haider <mohtashimhaider@mohtashims-MacBook-Air.local>
2024-09-23 15:28:12 +0600

    implementing money adding

--- js/home.js
+++ js/home.js
@@ -3,9 +3,24 @@ document.querySelector('#add-money').addEventListener('click',(e)=>{
     e.preventDefault();
 
     //extracting values
-    
-    const inputMoney = document.getElementById('input-add-money').value
+    const inputMoney = parseFloat(document.getElementById('input-add-money').value);
     const inputPin = document.getElementById('input-pin').value;
+
+    //simple validation
+
+    if(inputPin==='1234'){
+    const currentBalance = parseFloat(document.getElementById('current-balance').innerText);
+    const newBalance = currentBalance+inputMoney;
+    //setting new balance to the balance on the top
+    document.getElementById('current-balance').innerText=newBalance;
+    
+    
+    
+
+
+    }else{
+        alert('Failed')
+    }
 
     
 })
