df6056e2dc256b8e58b95d5fffcd133651bc600c
mohtashim haider <mohtashimhaider@mohtashims-MacBook-Air.local>
2024-09-23 15:00:23 +0600

    added partial event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home added')
+
+document.querySelector('#add-money').addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    //extracting values
+    
+    const inputMoney = document.getElementById('input-add-money').value
+    const inputPin = document.getElementById('input-pin').value;
+
+    
+})
