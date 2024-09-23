334259929c3a98d8dd73e80eeefbd97864be77d8
mohtashim haider <mohtashimhaider@mohtashims-MacBook-Air.local>
2024-09-23 13:21:50 +0600

    added simple login handler

---
+++ js/login.js
@@ -0,0 +1,11 @@
+//getting button
+document.getElementById('login-button').addEventListener('click',(e)=>{
+    e.preventDefault(); //preventing default behave 
+
+    //getting input from the form
+    const phoneNumber = document.querySelector('#phone-number').value;
+    const pinNumber = document.querySelector('#pin').value;
+    
+})
+
+
